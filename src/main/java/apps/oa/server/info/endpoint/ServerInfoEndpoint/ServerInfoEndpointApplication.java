549cae5355808d0ee854919e2b7398dc733ff50e
Om Ambarkar <om.ambarkar@gmail.com>
2025-03-15 12:40:23 +0530

    Uptime fixed, enabled Scheduling in Main

--- src/main/java/apps/oa/server/info/endpoint/ServerInfoEndpoint/ServerInfoEndpointApplication.java
+++ src/main/java/apps/oa/server/info/endpoint/ServerInfoEndpoint/ServerInfoEndpointApplication.java
@@ -2,8 +2,10 @@ package apps.oa.server.info.endpoint.ServerInfoEndpoint;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ServerInfoEndpointApplication {
 
 	public static void main(String[] args) {
